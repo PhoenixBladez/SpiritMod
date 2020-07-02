25d124c91399b3d92bd6f22e874b1cf07b0a00c0
Colman Davenport <colmandavenport@gmail.com>
2020-07-02 10:55:26 -0400

    fix gambler lag

--- Items/Consumable/GamblerChests/PlatinumChest.cs
+++ Items/Consumable/GamblerChests/PlatinumChest.cs
@@ -9,44 +9,37 @@ namespace SpiritMod.Items.Consumable.GamblerChests
 		public override void SetStaticDefaults()
 		{
 			DisplayName.SetDefault("Platinum Strongbox");
-			Tooltip.SetDefault("'May contain a fortune'");
+			Tooltip.SetDefault("Right click to open\n'May contain a fortune'");
 		}
+
 		public override void SetDefaults()
 		{
 			item.width = 40;
 			item.height = 40;
-			item.value = 500000;
+			item.value = Item.buyPrice(gold: 50);
 			item.rare = ItemRarityID.LightRed;
 			item.maxStack = 30;
 			item.autoReuse = true;
 		}
 
-		public override bool CanRightClick()
-		{
-			return true;
-		}
+		public override bool CanRightClick() => true;
+
 		public override void RightClick(Player player)
 		{
 			int[] lootTable = { 1, 10, 50, 100, 500, 1000, 5000, 10000, 50000, 100000, 250000, 500000, 1000000, 5000000 };
 
 			int loot = Main.rand.Next(lootTable.Length);
-			int amount = (lootTable[loot]);
-			while(amount >= 1000000) {
-				player.QuickSpawnItem(74);
-				amount -= 1000000;
-			}
-			while(amount >= 10000) {
-				player.QuickSpawnItem(73);
-				amount -= 10000;
-			}
-			while(amount >= 100) {
-				player.QuickSpawnItem(72);
-				amount -= 100;
-			}
-			player.QuickSpawnItem(71, amount);
+			int amount = lootTable[loot];
+			player.QuickSpawnItem(ItemID.PlatinumCoin, amount / 1000000);
+			amount %= 1000000;
+			player.QuickSpawnItem(ItemID.GoldCoin, amount / 10000);
+			amount %= 10000;
+			player.QuickSpawnItem(ItemID.SilverCoin, amount / 100);
+			amount %= 100;
+			player.QuickSpawnItem(ItemID.CopperCoin, amount);
 
 			if(Main.rand.Next(20) == 1) {
-				player.QuickSpawnItem(3817, Main.rand.Next(30));
+				player.QuickSpawnItem(ItemID.DefenderMedal, Main.rand.Next(30));
 			}
 		}
 	}
