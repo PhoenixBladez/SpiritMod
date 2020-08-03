87f9c5247618c66512c865b0c7b56f65984673f9
PhoenixBladez <aryanandabhay@gmail.com>
2020-08-02 23:40:18 -0700

    Additions
    - Added new Acid Armor, Map Resprites
    - Added WW Painting
    - Added new Briar Ambient Objects
    - Added Noxophyll critter

---
+++ Tiles/Ambient/Briar/Blaststone_Grave.cs
@@ -0,0 +1,37 @@
+
+using Microsoft.Xna.Framework;
+
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+using Terraria.ObjectData;
+
+namespace SpiritMod.Tiles.Ambient.Briar
+{
+	public class Blaststone_Grave : ModTile
+	{
+		public override void SetDefaults()
+		{
+			Main.tileFrameImportant[Type] = true;
+			Main.tileNoAttach[Type] = true;
+			Main.tileLavaDeath[Type] = true;
+			TileObjectData.newTile.CopyFrom(TileObjectData.Style3x2);
+			TileObjectData.newTile.Height = 2;
+			TileObjectData.newTile.Width = 2;
+			TileObjectData.newTile.CoordinateHeights = new int[]
+			{
+			16,
+			16
+			};
+			TileObjectData.addTile(Type);
+            dustType = 1;
+			ModTranslation name = CreateMapEntryName();
+			name.SetDefault("Gravestone");
+			AddMapEntry(new Color(107, 90, 64), name);
+		}
+		public override void SetDrawPositions(int i, int j, ref int width, ref int offsetY, ref int height)
+		{
+			offsetY = 2;
+		}
+	}
+}
---
+++ Tiles/Ambient/Briar/SkullGrave.cs
@@ -0,0 +1,39 @@
+
+using Microsoft.Xna.Framework;
+
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+using Terraria.ObjectData;
+
+namespace SpiritMod.Tiles.Ambient.Briar
+{
+	public class SkullGrave : ModTile
+	{
+		public override void SetDefaults()
+		{
+			Main.tileFrameImportant[Type] = true;
+			Main.tileNoAttach[Type] = true;
+			Main.tileLavaDeath[Type] = true;
+			TileObjectData.newTile.CopyFrom(TileObjectData.Style3x2);
+			TileObjectData.newTile.Height = 4;
+			TileObjectData.newTile.Width = 2;
+			TileObjectData.newTile.CoordinateHeights = new int[]
+			{
+			16,
+			16,
+			16,
+			16
+			};
+			TileObjectData.addTile(Type);
+            dustType = 7;
+			ModTranslation name = CreateMapEntryName();
+			name.SetDefault("Gravestone");
+			AddMapEntry(new Color(107, 90, 64), name);
+		}
+		public override void SetDrawPositions(int i, int j, ref int width, ref int offsetY, ref int height)
+		{
+			offsetY = 2;
+		}
+	}
+}
