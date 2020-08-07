a1905bd0fa643a804c03ff8f1206952e321f2c93
PhoenixBladez <aryanandabhay@gmail.com>
2020-08-07 15:17:30 -0700

    Update AsteroidChest.cs

--- Tiles/Furniture/SpaceJunk/AsteroidChest.cs
+++ Tiles/Furniture/SpaceJunk/AsteroidChest.cs
@@ -73,104 +73,123 @@ namespace SpiritMod.Tiles.Furniture.SpaceJunk
 			Chest.DestroyChest(i, j);
 		}
 
-		public override bool NewRightClick(int i, int j)
-		{
-			Player player = Main.LocalPlayer;
-			Tile tile = Main.tile[i, j];
-			Main.mouseRightRelease = false;
-			int left = i;
-			int top = j;
-			if (tile.frameX % 36 != 0) {
-				left--;
-			}
-			if (tile.frameY != 0) {
-				top--;
-			}
-			if (player.sign >= 0) {
-				Main.PlaySound(SoundID.MenuClose);
-				player.sign = -1;
-				Main.editSign = false;
-				Main.npcChatText = "";
-			}
-			if (Main.editChest) {
-				Main.PlaySound(SoundID.MenuTick);
-				Main.editChest = false;
-				Main.npcChatText = "";
-			}
-			if (player.editedChestName) {
-				NetMessage.SendData(MessageID.SyncPlayerChest, -1, -1, NetworkText.FromLiteral(Main.chest[player.chest].name), player.chest, 1f, 0f, 0f, 0, 0, 0);
-				player.editedChestName = false;
-			}
-			if (Main.netMode == NetmodeID.Server) {
-				if (left == player.chestX && top == player.chestY && player.chest >= 0) {
-					player.chest = -1;
-					Recipe.FindRecipes();
-					Main.PlaySound(SoundID.MenuClose);
-				}
-				else {
-					NetMessage.SendData(MessageID.RequestChestOpen, -1, -1, null, left, (float)top, 0f, 0f, 0, 0, 0);
-					Main.stackSplit = 600;
-				}
-			}
-			else {
-				int chest = Chest.FindChest(left, top);
-				if (chest >= 0) {
-					Main.stackSplit = 600;
-					if (chest == player.chest) {
-						player.chest = -1;
-						Main.PlaySound(SoundID.MenuClose);
-					}
-					else {
-						player.chest = chest;
-						Main.playerInventory = true;
-						Main.recBigList = false;
-						player.chestX = left;
-						player.chestY = top;
-						Main.PlaySound(player.chest < 0 ? SoundID.MenuOpen : SoundID.MenuTick);
-					}
-					Recipe.FindRecipes();
-				}
-			}
+   public override bool NewRightClick(int i, int j)
+        {
+            Player player = Main.LocalPlayer;
+            Tile tile = Main.tile[i, j];
+            Main.mouseRightRelease = false;
+            int left = i;
+            int top = j;
+            if (tile.frameX % 36 != 0)
+            {
+                left--;
+            }
+            if (tile.frameY != 0)
+            {
+                top--;
+            }
+            if (player.sign >= 0)
+            {
+                Main.PlaySound(SoundID.MenuClose);
+                player.sign = -1;
+                Main.editSign = false;
+                Main.npcChatText = "";
+            }
+            if (Main.editChest)
+            {
+                Main.PlaySound(SoundID.MenuTick);
+                Main.editChest = false;
+                Main.npcChatText = "";
+            }
+            if (player.editedChestName)
+            {
+                NetMessage.SendData(MessageID.SyncPlayerChest, -1, -1, NetworkText.FromLiteral(Main.chest[player.chest].name), player.chest, 1f, 0f, 0f, 0, 0, 0);
+                player.editedChestName = false;
+            }
+            if (Main.netMode == NetmodeID.MultiplayerClient)
+            {
+                if (left == player.chestX && top == player.chestY && player.chest >= 0)
+                {
+                    player.chest = -1;
+                    Recipe.FindRecipes();
+                    Main.PlaySound(SoundID.MenuClose);
+                }
+                else
+                {
+                    NetMessage.SendData(MessageID.RequestChestOpen, -1, -1, null, left, top, 0f, 0f, 0, 0, 0);
+                    Main.stackSplit = 600;
+                }
+            }
+            else
+            {
+                int chest = Chest.FindChest(left, top);
+                if (chest >= 0)
+                {
+                    Main.stackSplit = 600;
+                    if (chest == player.chest)
+                    {
+                        player.chest = -1;
+                        Main.PlaySound(SoundID.MenuClose);
+                    }
+                    else
+                    {
+                        player.chest = chest;
+                        Main.playerInventory = true;
+                        Main.recBigList = false;
+                        player.chestX = left;
+                        player.chestY = top;
+                        Main.PlaySound(player.chest < 0 ? SoundID.MenuOpen : SoundID.MenuTick);
+                    }
+                    Recipe.FindRecipes();
+                }
+            }
+            return true;
+        }
+        public override void SetDrawPositions(int i, int j, ref int width, ref int offsetY, ref int height)
+        {
+            offsetY = 2;
+        }
+        public override void MouseOver(int i, int j)
+        {
+            Player player = Main.LocalPlayer;
+            Tile tile = Main.tile[i, j];
+            int left = i;
+            int top = j;
+            if (tile.frameX % 36 != 0)
+            {
+                left--;
+            }
+            if (tile.frameY != 0)
+            {
+                top--;
+            }
+            int chest = Chest.FindChest(left, top);
+            player.showItemIcon2 = -1;
+            if (chest < 0)
+            {
+                player.showItemIconText = Lang.chestType[0].Value;
+            }
+            else
+            {
+                player.showItemIconText = Main.chest[chest].name.Length > 0 ? Main.chest[chest].name : "Asteroid Chest";
+                if (player.showItemIconText == "Asteroid Chest")
+                {
+                    player.showItemIcon2 = ModContent.ItemType<Items.Placeable.Furniture.AsteroidChest>();
+                    player.showItemIconText = "";
+                }
+                player.noThrow = 2;
+            player.showItemIcon = true;
+        }
 
-			return true;
-		}
-
-		public override void MouseOver(int i, int j)
-		{
-			Player player = Main.LocalPlayer;
-			Tile tile = Main.tile[i, j];
-			int left = i;
-			int top = j;
-			if (tile.frameX % 36 != 0) {
-				left--;
-			}
-			if (tile.frameY != 0) {
-				top--;
-			}
-			int chest = Chest.FindChest(left, top);
-			player.showItemIcon2 = -1;
-			if (chest < 0) {
-				player.showItemIconText = Lang.chestType[0].Value;
-			}
-			else {
-				player.showItemIconText = Main.chest[chest].name.Length > 0 ? Main.chest[chest].name : "Asteroid Chest";
-				if (player.showItemIconText == "Asteroid Chest") {
-					player.showItemIcon2 = ModContent.ItemType<Items.Placeable.Furniture.AsteroidChest>();
-					player.showItemIconText = "";
-				}
-			}
-			player.noThrow = 2;
-			player.showItemIcon = true;
-		}
-
-		public override void MouseOverFar(int i, int j)
-		{
-			MouseOver(i, j);
-			Player player = Main.LocalPlayer;
-			if (player.showItemIconText == "") {
-				player.showItemIcon = false;
-				player.showItemIcon2 = 0;
-			}
-		}
-	}
+        public override void MouseOverFar(int i, int j)
+        {
+            MouseOver(i, j);
+            Player player = Main.LocalPlayer;
+            if (player.showItemIconText == "")
+            {
+                player.showItemIcon = false;
+                player.showItemIcon2 = 0;
+            }
+        }
+    }
 }
