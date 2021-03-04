9ad7803bd34852d82c0773c10edddd5358e7c9f2
Graydee2 <32801528+Graydee2@users.noreply.github.com>
2021-03-03 19:49:48 -0500

    Incomplete club sandwich (plz add images and commit asap)

---
+++ Items/ClubMealFour.cs
@@ -0,0 +1,42 @@
+using Microsoft.Xna.Framework;
+using System.Collections.Generic;
+using System.Linq;
+using Terraria;
+using Terraria.DataStructures;
+using Terraria.ID;
+using SpiritMod.Buffs;
+using Terraria.ModLoader;
+using static Terraria.ModLoader.ModContent;
+
+namespace SpiritMod.Items
+{
+	public class ClubMealFour : ModItem
+	{
+		public override void SetStaticDefaults()
+		{
+			DisplayName.SetDefault("Club Meal");
+			Tooltip.SetDefault("You shouldn't see this");
+		}
+		public override void SetDefaults()
+		{
+			item.width = 8;
+			item.height = 8;
+			item.maxStack = 1;
+		}
+
+		public override bool ItemSpace(Player player)
+		{
+			return true;
+		}
+		public override void GrabRange(Player player, ref int grabRange)
+		{
+			grabRange = 0;
+		}
+		public override bool OnPickup(Player player)
+		{
+			player.AddBuff(BuffID.WellFed, 120);
+			Main.PlaySound(SoundID.Item, (int)player.position.X, (int)player.position.Y, 139, 2);
+			return false;
+		}
+	}
+}
