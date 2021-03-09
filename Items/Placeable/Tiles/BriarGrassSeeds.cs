315b5d631f24d0fd4a99567da65702e88582d196
Trivaxy <itsmeomar2003@yahoo.com>
2021-03-09 03:40:15 +0200

    move briar grass seeds to proper namespace

--- Items/Placeable/Tiles/BriarGrassSeeds.cs
+++ Items/Placeable/Tiles/BriarGrassSeeds.cs
@@ -3,7 +3,7 @@ using Terraria;
 using Terraria.ID;
 using Terraria.ModLoader;
 
-namespace SpiritMod.Items.Placeable
+namespace SpiritMod.Items.Placeable.Tiles
 {
 	public class BriarGrassSeeds : ModItem
 	{
