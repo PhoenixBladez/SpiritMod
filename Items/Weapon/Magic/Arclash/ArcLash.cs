02d3f1a8715073957bbadb345594b7f100f20f98
zoroarkcity <zoroarkcity2002@gmail.com>
2021-03-13 16:15:29 -0800

    arclash polish

--- Items/Weapon/Magic/Arclash/ArcLash.cs
+++ Items/Weapon/Magic/Arclash/ArcLash.cs
@@ -11,63 +11,50 @@ namespace SpiritMod.Items.Weapon.Magic.Arclash
 		public override void SetStaticDefaults()
 		{
 			DisplayName.SetDefault("Arc Lash");
-			Tooltip.SetDefault("Does more damage towards the end of it's cycle");
+			Tooltip.SetDefault("Does more damage towards the end of its cycle");
 		}
-
-		float arcProgress = 0;
 		public override void SetDefaults()
 		{
 			item.damage = 12;
 			item.magic = true;
-			item.mana = 1;
+			item.mana = 6;
 			item.width = 44;
 			item.height = 46;
 			item.channel = true;
-			item.useTime = 2;
-			item.useAnimation = 2;
+			item.useTime = 30;
+			item.useAnimation = 30;
 			item.useStyle = ItemUseStyleID.HoldingOut;
 			Item.staff[item.type] = true;
 			item.noMelee = true;
 			item.knockBack = 3;
-			item.value = Item.sellPrice(0, 01, 10, 0);
-			item.rare = 2;
+			item.value = Item.sellPrice(0, 1, 10, 0);
+			item.UseSound = SoundID.Item15;
+			item.rare = ItemRarityID.Green;
 			item.autoReuse = true;
 			item.shoot = ModContent.ProjectileType<ArcLashProj>();
 			item.shootSpeed = 8;
 		}
-		Color color = Color.Green;
         public override bool Shoot(Player player, ref Vector2 position, ref float speedX, ref float speedY, ref int type, ref int damage, ref float knockBack)
         {
-			arcProgress++;
-			arcProgress *= 1.07f;
-			if (arcProgress >= 50)
-			{
-				int red = Main.rand.Next(60, 255);
-				int green = Main.rand.Next(60, 255);
-				int blue = Main.rand.Next(60, 255);
-				color = new Color(red, green, blue);
-			}
-			arcProgress%= 50;
 			Vector2 direction = new Vector2(speedX,speedY);
-			position+= direction * (7 + (arcProgress / 30));
-			int proj = Projectile.NewProjectile(position + (direction * arcProgress * 0.66f), Vector2.Zero, type, damage * (int)Math.Pow(arcProgress, 0.3f), knockBack, player.whoAmI);
-			if (Main.netMode != NetmodeID.Server)
-			{
-				SpiritMod.primitives.CreateTrail(new ArclashPrimTrail(Main.projectile[proj], position + (direction.RotatedBy(-0.3f - (arcProgress / 250f)) * arcProgress), position + (direction.RotatedBy(0.3f + (arcProgress / 250f)) * arcProgress), position, position, arcProgress, color));
-			}
+			position+= direction * 7;
+			Projectile.NewProjectile(position, Vector2.Zero, type, damage, knockBack, player.whoAmI);
 			return false;
         }
-       /* public override void HoldItem(Player player)
-		{
-			if (!player.channel && player.itemAnimation == 0) arcProgress = 0;
-		}*/
+
+		public override bool CanUseItem(Player player) => player.ownedProjectileCounts[item.shoot] == 0;
 	}
 	public class ArcLashProj : ModProjectile
     {
-        public override void SetStaticDefaults()
-        {
-            DisplayName.SetDefault("Arc Lash");
-        }
+		public override void SetStaticDefaults() => DisplayName.SetDefault("Arc Lash");
+
+		private readonly int cycletime = 40;
+		private readonly float lengthmult = 50;
+		private ref float Progress => ref projectile.ai[0];
+
+		private ref float Angle => ref projectile.ai[1];
+
+		Player Owner => Main.player[projectile.owner];
 
         public override void SetDefaults()
         {
@@ -76,16 +63,43 @@ namespace SpiritMod.Items.Weapon.Magic.Arclash
             projectile.aiStyle = -1;
             projectile.magic = true;
             projectile.penetrate = -1;
-            projectile.hostile = false;
             projectile.friendly = true;
             projectile.tileCollide = false;
-            projectile.timeLeft = 5;
-            projectile.extraUpdates = 0;
+            projectile.timeLeft = cycletime;
             projectile.alpha = 255;
         }
 
         public override void AI()
         {
+			if (!Owner.channel)
+				projectile.Kill();
+
+			if (Main.LocalPlayer == Owner) {
+				Angle = Owner.AngleTo(Main.MouseWorld);
+				projectile.netUpdate = true;
+			}
+
+			Progress = lengthmult * (cycletime - projectile.timeLeft)/cycletime;
+			Owner.ChangeDir(Math.Sign(projectile.Center.X - Owner.Center.X));
+			Owner.itemRotation = MathHelper.WrapAngle(Angle - ((Owner.direction < 0) ? MathHelper.Pi : 0));
+			Owner.itemTime = 2;
+			Owner.itemAnimation = 2;
+
+			if(Progress == cycletime/2) {
+				Owner.CheckMana(Owner.HeldItem, Owner.HeldItem.mana, true);
+				Main.PlaySound(Owner.HeldItem.UseSound, Owner.MountedCenter);
+			}
+
+			Vector2 direction = Vector2.UnitX * 8;
+			direction = direction.RotatedBy(projectile.ai[1]);
+			Vector2 position = Owner.MountedCenter + (direction * (7 + (Progress / 30)));
+			projectile.Center = position + (direction * Progress * 0.66f);
+
+			if (Main.netMode != NetmodeID.Server && ++projectile.localAI[0] == 1) {
+				SpiritMod.primitives.CreateTrail(new ArclashPrimTrail(projectile));
+			}
         }
-    }
+
+		public override void ModifyHitNPC(NPC target, ref int damage, ref float knockback, ref bool crit, ref int hitDirection) => damage = (int)(Owner.HeldItem.damage * (int)Math.Pow(Progress, 0.3f));
+	}
 }
--- Prim/ArclashPrimTrail.cs
+++ Prim/ArclashPrimTrail.cs
@@ -1,33 +1,20 @@
-﻿using Terraria.ModLoader;
-using Microsoft.Xna.Framework.Graphics;
+﻿using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework;
 using Terraria;
 using System.Collections.Generic;
 using System.Linq;
 using System;
-using static Terraria.ModLoader.ModContent;
-using System.Reflection;
 
 namespace SpiritMod.Prim
 {
     class ArclashPrimTrail : PrimTrail
     {
-        public Vector2 _c1;
-        public Vector2 _c2;
-        public Vector2 _endpt;
-        public Vector2 _startpt;
-        public float _arcProgress;
-        public ArclashPrimTrail(Projectile projectile, Vector2 c1, Vector2 c2, Vector2 endpos, Vector2 startpos, float arcProgress, Color color) : base()
+        private float _arcProgress;
+        public ArclashPrimTrail(Projectile projectile) : base()
         {
-             _projectile = projectile;
+            _projectile = projectile;
 			_entitytype = _projectile.type;
 			_drawtype = PrimTrailManager.DrawProjectile;
-            _c1 = c1;
-            _c2 = c2;
-            _endpt = endpos;
-            _startpt = startpos;
-            _arcProgress = arcProgress;
-            _color = color;
         }
          public override void SetDefaults()
         {
@@ -63,8 +50,8 @@ namespace SpiritMod.Prim
                     if (i != _points.Count - 1)
                     {
                         widthVar = _width;
-                        Color c = Color.White;
-                        Color CBT = Color.White;
+                        Color c = Color.Cyan;
+                        Color CBT = Color.Cyan;
                         Vector2 normal = CurveNormal(_points, i);
                         Vector2 normalAhead = CurveNormal(_points, i + 1);
                         float j = (((float)_points.Count) + ((float)(Math.Sin(_counter / 10f)) * 1) - i * 0.1f) / ((float)_points.Count);
@@ -89,26 +76,31 @@ namespace SpiritMod.Prim
                 }
             }
         }
-        public override void SetShaders()
-        {
-            PrepareShader(SpiritMod.ArcLashShader, "MainPS", _arcProgress / 50, _color);
-        }
-        public override void OnUpdate()
-        {
-            _counter++;
+		public override void SetShaders() => PrepareShader(SpiritMod.ArcLashShader, "MainPS", _arcProgress / 50, Color.Red);
+
+		public override void OnUpdate()
+		{
+			_arcProgress = _projectile.ai[0];
+			Vector2 direction = Vector2.UnitX * 8;
+			direction = direction.RotatedBy(_projectile.ai[1]);
+			Vector2 position = Main.player[_projectile.owner].MountedCenter + (direction * (7 + (_arcProgress / 30)));
+			Vector2 _c1 = position + (direction.RotatedBy(-0.3f - (_arcProgress / 250f)) * _arcProgress);
+			Vector2 _c2 = position + (direction.RotatedBy(0.3f + (_arcProgress / 250f)) * _arcProgress);
+			_counter++;
             _noOfPoints = _points.Count() * 6;
             if ((!_projectile.active && _projectile != null) || _destroyed)
             {
                 OnDestroy();
             }
-            else if (_noOfPoints == 0)
+            else
             {
-                List<Vector2> _points2 = Helpers.GetBezier(_endpt, _startpt, _c1, _c2, _arcProgress + 6);
+				_points.Clear();
+                List<Vector2> _points2 = Helpers.GetBezier(position, position, _c1, _c2, _arcProgress + 6);
                 foreach(Vector2 point in _points2)
                 {
                     Vector2 point2 = point;
                     point2.X += Main.rand.Next(-2,2);
-                     point2.Y += Main.rand.Next(-2,2);
+                    point2.Y += Main.rand.Next(-2,2);
                     _points.Add(point2);
                 }
             }
