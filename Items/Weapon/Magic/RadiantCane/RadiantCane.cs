786c98272c3eb715e238963ae9f0261eefd133cb
zoroarkcity <zoroarkcity2002@gmail.com>
2021-03-20 20:05:47 -0700

    schtewpid

--- Items/Weapon/Magic/RadiantCane/RadiantCane.cs
+++ Items/Weapon/Magic/RadiantCane/RadiantCane.cs
@@ -12,7 +12,7 @@ namespace SpiritMod.Items.Weapon.Magic.RadiantCane
 	{
 		public override void SetStaticDefaults()
 		{
-			DisplayName.SetDefault("Sunrise Sceptre");
+			DisplayName.SetDefault("Sunrise Scepter");
 			Tooltip.SetDefault("Conjures a slow, controllable sun orb");
 			SpiritGlowmask.AddGlowMask(item.type, Texture + "_glow");
         }
