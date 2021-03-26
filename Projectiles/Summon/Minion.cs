64e1a89d5de83453aaf8fd7b39785f21031c20ea
Alekshhh <realmafioso3@gmail.com>
2021-03-26 21:34:29 +0300

    im dumb

---
+++ Projectiles/Summon/Minion.cs
@@ -0,0 +1,20 @@
+using Terraria.ModLoader;
+
+namespace SpiritMod.Projectiles.Summon
+{
+	public abstract class Minion : ModProjectile
+	{
+		public override void AI()
+		{
+			CheckActive();
+			Behavior();
+			SelectFrame();
+		}
+
+		public abstract void CheckActive();
+
+		public abstract void Behavior();
+
+		public abstract void SelectFrame();
+	}
+}
