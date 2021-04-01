320ee4bba5b5ac4da88b8950516a4e6594de1ed7
zoroarkcity <zoroarkcity2002@gmail.com>
2021-03-31 17:22:43 -0700

    Update ScarabBow.cs

--- Items/Weapon/Bow/AdornedBow/ScarabBow.cs
+++ Items/Weapon/Bow/AdornedBow/ScarabBow.cs
@@ -75,15 +75,15 @@ namespace SpiritMod.Items.Weapon.Bow.AdornedBow
 
 		protected override void Charging() => AdjustDirection();
 
-		private static readonly BasicEffect effect = new BasicEffect(Main.instance.GraphicsDevice);
-
 		public override bool PreDraw(SpriteBatch spriteBatch, Color lightColor)
 		{
 			//if (firing)
 				//return false;
 			spriteBatch.End();
 			Player player = Main.player[projectile.owner];
-			effect.VertexColorEnabled = true;
+			BasicEffect effect = new BasicEffect(Main.instance.GraphicsDevice) {
+				VertexColorEnabled = true
+			};
 			Color color = Color.Lerp(new Color(158, 255, 253), lightColor, 0.3f);
 			ArrowDraw.DrawArrowBasic(effect, player.Center + direction * 20, direction.ToRotation() + 3.14f, 0 - LerpFloat(0, maxVelocity, charge) * 3, 12,
 				color * 0.3f, 0 - LerpFloat(0.7f, 0.4f, charge), 0 - LerpFloat(minVelocity, maxVelocity, charge) * 2);
