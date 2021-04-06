458f311ca9aaaf0a1a89af3c5fcc6e9c2cd32e43
Trivaxy <itsmeomar2003@yahoo.com>
2021-04-06 15:35:38 +0300

    Revert "Pulling my shit. You can deal with the broken shit yourself."
    This reverts commit 8bd01e92c1c7ab438b164f1b68db474591411aa4.

--- SpiritDetours.cs
+++ SpiritDetours.cs
@@ -60,6 +60,21 @@ namespace SpiritMod
 			self.GetSpiritPlayer().DoubleTapEffects(keyDir);
 		}
 
+		//Additive drawing stuff. Optimize this later?
+		private static void DrawAdditive(On.Terraria.Main.orig_DrawDust orig, Main self)
+		{
+			orig(self);
+			Main.spriteBatch.Begin(default, BlendState.Additive, default, default, default, default, Main.GameViewMatrix.ZoomMatrix);
+
+			for (int k = 0; k < Main.maxProjectiles; k++) //projectiles
+				if (Main.projectile[k].active && Main.projectile[k].modProjectile is IDrawAdditive) (Main.projectile[k].modProjectile as IDrawAdditive).DrawAdditive(Main.spriteBatch);
+
+			for (int k = 0; k < Main.maxNPCs; k++) //NPCs
+				if (Main.npc[k].active && Main.npc[k].modNPC is IDrawAdditive) (Main.npc[k].modNPC as IDrawAdditive).DrawAdditive(Main.spriteBatch);
+
+			Main.spriteBatch.End();
+		}
+
 		public static void DrawParticles(On.Terraria.Main.orig_DrawInterface orig, Main self, GameTime gameTime)
 		{
 			Main.spriteBatch.Begin(default, default, default, default, default, default, Main.GameViewMatrix.ZoomMatrix);
