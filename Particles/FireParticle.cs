db6ce6fc1153c75321e0dc06c14e5fbdcf3d3d97
Trivaxy <itsmeomar2003@yahoo.com>
2021-04-06 22:51:04 +0300

    fix FireParticles

--- Particles/FireParticle.cs
+++ Particles/FireParticle.cs
@@ -8,32 +8,29 @@ namespace SpiritMod.Particles
 	{
 		private static readonly int MaxTime = 540;
 
-		private Vector2 centerPlusPosition;
-		private Vector2 spawnPosition;
-
 		public override void Update()
 		{
 			if (TimeActive >= MaxTime)
 				Kill();
 
+			Velocity *= 0.9978f;
 			Color = Color.White * (float)Math.Sin(MathHelper.TwoPi * ((MaxTime - TimeActive) / (float)MaxTime)) * 0.85f;
 		}
 
-		public override float SpawnChance() => Main.LocalPlayer.ZoneMeteor || Main.LocalPlayer.ZoneUnderworldHeight ? 0.5f : 0;
+		public override float SpawnChance() => Main.LocalPlayer.ZoneMeteor || Main.LocalPlayer.ZoneUnderworldHeight ? 0.62f : 0;
 
 		public override void OnSpawnAttempt()
 		{
 			FireParticle fireParticle = new FireParticle();
 
-			Vector2 startingPosition = new Vector2(Main.LocalPlayer.Center.X + Main.screenWidth / 2 * (Main.rand.NextBool() ? 1 : -1), Main.LocalPlayer.Center.Y + Main.screenHeight);
+			Vector2 screenCenter = Main.screenPosition + new Vector2(Main.screenWidth / 2, Main.screenHeight / 2);
+			Vector2 startingPosition = new Vector2(Main.rand.NextFloat(screenCenter.X - Main.screenWidth * 2, screenCenter.X + Main.screenWidth * 2), Main.screenPosition.Y + Main.screenHeight * 2);
+
 			fireParticle.Position = startingPosition;
 			fireParticle.Velocity = new Vector2(Main.windSpeed * 2f, Main.rand.NextFloat(-2, -6f));
 			fireParticle.Rotation = Main.rand.NextFloat(MathHelper.PiOver4);
 			fireParticle.Scale = Main.rand.NextFloat(0.3f, 0.85f);
 			fireParticle.Color = Color.White;
-
-			fireParticle.centerPlusPosition = Main.LocalPlayer.Center + startingPosition;
-			fireParticle.spawnPosition = startingPosition;
 
 			ParticleHandler.SpawnParticle(fireParticle);
 		}
