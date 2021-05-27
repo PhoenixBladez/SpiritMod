22f84cfca5303984972ea378f52ed333a68d8517
GabeHasWon <gabriel@guevarameyer.com>
2021-05-27 09:11:32 -0500

    Feral Hunter cleanup / fixes?

--- NPCs/Reach/Reachman.cs
+++ NPCs/Reach/Reachman.cs
@@ -20,7 +20,7 @@ namespace SpiritMod.NPCs.Reach
 		public override void SetDefaults()
 		{
 			npc.width = 36;
-			npc.height = 44;
+			npc.height = 54;
 			npc.damage = 22;
 			npc.defense = 8;
 			npc.lifeMax = 66;
@@ -28,13 +28,15 @@ namespace SpiritMod.NPCs.Reach
 			npc.buffImmune[BuffID.Poisoned] = true;
 			npc.DeathSound = SoundID.NPCDeath2;
 			npc.value = 90f;
-			drawOffsetY = 4;
 			npc.knockBackResist = .34f;
 			npc.aiStyle = 3;
-			aiType = NPCID.SnowFlinx;
-            banner = npc.type;
+
+			//drawOffsetY = 4;
+			aiType = NPCID.Zombie;
+			banner = npc.type;
             bannerItem = ModContent.ItemType<Items.Banners.ReachmanBanner>();
         }
+
 		public override void FindFrame(int frameHeight)
 		{
 			npc.frameCounter += 0.10f;
@@ -42,56 +44,47 @@ namespace SpiritMod.NPCs.Reach
 			int frame = (int)npc.frameCounter;
 			npc.frame.Y = frame * frameHeight;
 		}
+
 		public override float SpawnChance(NPCSpawnInfo spawnInfo)
 		{
 			Player player = spawnInfo.player;
-			if (!(player.ZoneTowerSolar || player.ZoneTowerVortex || player.ZoneTowerNebula || player.ZoneTowerStardust) && ((!Main.pumpkinMoon && !Main.snowMoon)) && !Main.eclipse  && (SpawnCondition.GoblinArmy.Chance == 0)) {
+			if (!(player.ZoneTowerSolar || player.ZoneTowerVortex || player.ZoneTowerNebula || player.ZoneTowerStardust) && !Main.pumpkinMoon && !Main.snowMoon && !Main.eclipse  && (SpawnCondition.GoblinArmy.Chance == 0))
 				return spawnInfo.player.GetSpiritPlayer().ZoneReach ? 1.7f : 0f;
-			}
 			return 0f;
 		}
-		public override void AI()
-		{
-			Lighting.AddLight((int)((npc.position.X + (float)(npc.width / 2)) / 16f), (int)((npc.position.Y + (float)(npc.height / 2)) / 16f), 0.46f, 0.32f, .1f);
-		}
+
+		public override void AI() => Lighting.AddLight((int)(npc.Center.X / 16f), (int)(npc.Center.Y / 16f), 0.46f, 0.32f, .1f);
+
 		public override void NPCLoot()
 		{
-			if (Main.rand.Next(20) == 1) {
+			if (Main.rand.Next(20) == 1)
 				Item.NewItem((int)npc.position.X, (int)npc.position.Y, npc.width, npc.height, ModContent.ItemType<SanctifiedStabber>());
-			}
             if (Main.rand.NextBool(33))
-            {
                 Item.NewItem((int)npc.position.X, (int)npc.position.Y, npc.width, npc.height, ModContent.ItemType<CaesarSalad>());
-            }
-            if (!Main.dayTime) {
+            if (!Main.dayTime)
 				Item.NewItem((int)npc.position.X, (int)npc.position.Y, npc.width, npc.height, ModContent.ItemType<EnchantedLeaf>());
-			}
-
 		}
+
 		public override void OnHitPlayer(Player target, int damage, bool crit)
 		{
-			if (Main.rand.Next(10) == 0 && Main.expertMode) {
+			if (Main.rand.Next(10) == 0 && Main.expertMode)
 				target.AddBuff(148, 2000);
-			}
 		}
+
 		public override bool PreDraw(SpriteBatch spriteBatch, Color drawColor)
 		{
 			var effects = npc.direction == -1 ? SpriteEffects.None : SpriteEffects.FlipHorizontally;
-			spriteBatch.Draw(Main.npcTexture[npc.type], npc.Center - Main.screenPosition + new Vector2(0, npc.gfxOffY), npc.frame,
-							 drawColor, npc.rotation, npc.frame.Size() / 2, npc.scale, effects, 0);
+			spriteBatch.Draw(Main.npcTexture[npc.type], npc.Center - Main.screenPosition + new Vector2(0, npc.gfxOffY), npc.frame, drawColor, npc.rotation, npc.frame.Size() / 2, npc.scale, effects, 0);
 			return false;
 		}
-		public override void PostDraw(SpriteBatch spriteBatch, Color drawColor)
-		{
-			GlowmaskUtils.DrawNPCGlowMask(spriteBatch, npc, mod.GetTexture("NPCs/Reach/Reachman_Glow"));
-		}
+
+		public override void PostDraw(SpriteBatch spriteBatch, Color drawColor) => GlowmaskUtils.DrawNPCGlowMask(spriteBatch, npc, mod.GetTexture("NPCs/Reach/Reachman_Glow"));
+
 		public override void HitEffect(int hitDirection, double damage)
 		{
-			int d = 3;
-			int d1 = 7;
 			for (int k = 0; k < 30; k++) {
-				Dust.NewDust(npc.position, npc.width, npc.height, d, 2.5f * hitDirection, -2.5f, 0, Color.White, 0.7f);
-				Dust.NewDust(npc.position, npc.width, npc.height, d1, 2.5f * hitDirection, -2.5f, 0, default(Color), .34f);
+				Dust.NewDust(npc.position, npc.width, npc.height, 3, 2.5f * hitDirection, -2.5f, 0, Color.White, 0.7f);
+				Dust.NewDust(npc.position, npc.width, npc.height, 7, 2.5f * hitDirection, -2.5f, 0, default, .34f);
 			}
 
 			if (npc.life <= 0) {
