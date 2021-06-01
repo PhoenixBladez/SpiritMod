fcb3bd35ececf38dd28182ce97762e3c30b35992
GraydeeSi <wiseguy0002@gmail.com>
2021-06-01 13:35:25 -0700

    Update Starachnid.cs

--- NPCs/StarjinxEvent/Enemies/Starachnid/Starachnid.cs
+++ NPCs/StarjinxEvent/Enemies/Starachnid/Starachnid.cs
@@ -198,9 +198,7 @@ namespace SpiritMod.NPCs.StarjinxEvent.Enemies.Starachnid
 				Main.spriteBatch.Draw(Main.magicPixel, thread.StartPoint - Main.screenPosition, new Rectangle(0, 0, 1, 1), Color.Purple, (thread.EndPoint - thread.StartPoint).ToRotation(), new Vector2(0f, 1f), new Vector2(thread.Length * length, 5 * length), SpriteEffects.None, 0f);
 			}
 			StarThread thread2 = currentThread;
-			length = Math.Min(thread2.Counter / THREADGROWLERP, (thread2.Duration - thread2.Counter) / THREADGROWLERP);
-			length = Math.Min(length, 1);
-			Main.spriteBatch.Draw(Main.magicPixel, thread2.StartPoint - Main.screenPosition, new Rectangle(0, 0, 1, 1), Color.Cyan, (thread2.EndPoint - thread2.StartPoint).ToRotation(), new Vector2(0f, 1f), new Vector2(thread2.Length * length, 5 * length), SpriteEffects.None, 0f);
+			Main.spriteBatch.Draw(Main.magicPixel, thread2.StartPoint - Main.screenPosition, new Rectangle(0, 0, 1, 1), Color.Cyan, (thread2.EndPoint - thread2.StartPoint).ToRotation(), new Vector2(0f, 1f), new Vector2(thread2.Length * progress, 5), SpriteEffects.None, 0f);
 		}
 
 		private void UpdateThreads()
