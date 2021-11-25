c9d953bfcad8629ca02e9d646cf49070dc1dd27e
GabeHasWon <gabriel@guevarameyer.com>
2021-11-25 13:11:23 -0600

    pins on map woo

--- Utilities/SpiritDetours.cs
+++ Utilities/SpiritDetours.cs
@@ -47,11 +47,84 @@ namespace SpiritMod.Utilities
 			On.Terraria.Main.DrawBackgroundBlackFill += Main_DrawBackgroundBlackFill; //BackgroundItemManager.Draw()
 			On.Terraria.Main.Update += Main_Update; //BackgroundItemManager.Update()
 
+			On.Terraria.Main.DrawMap += Main_DrawMap;
+
 			Main.OnPreDraw += Main_OnPreDraw;
 
 			IL.Terraria.Player.ItemCheck += Player_ItemCheck;
 			IL.Terraria.WorldGen.hardUpdateWorld += WorldGen_hardUpdateWorld;
 			//IL.Terraria.Main.DoDraw += Main_DoDraw;
+		}
+
+		private static void Main_DrawMap(On.Terraria.Main.orig_DrawMap orig, Main self)
+		{
+			orig(self);
+
+			if (Main.mapEnabled && !Main.mapFullscreen) //Draw only on the minimap
+				DrawPinsOnMiniMap(); //Main.mapStyle == 0, fullscreen; == 1, minimap; == 2, overlay
+		}
+
+		private static void DrawPinsOnMiniMap()
+		{
+			var pins = ModContent.GetInstance<Items.Pins.PinWorld>().pins;
+
+			foreach (var pair in pins)
+			{
+				float scale = Main.mapMinimapScale;
+
+				Vector2 realPos = GetMiniMapPosition(pins.Get<Vector2>(pair.Key), scale);
+
+				if (scale > 1f)
+					scale = 1f;
+				scale *= Main.UIScale;
+
+				float drawScale = (scale * 0.5f + 1f) / 3f;
+				if (drawScale > 1f)
+					drawScale = 1f;
+
+				if (PointOnMinimap(realPos))
+					DrawOnMinimap((int)realPos.X, (int)realPos.Y, drawScale, ModContent.GetTexture($"SpiritMod/Items/Pins/Textures/Pin{pair.Key}Map"));
+			}
+		}
+
+		public static bool PointOnMinimap(Vector2 pos, float fluff = 4) => pos.X > (Main.miniMapX + fluff) && pos.X < (Main.miniMapX + Main.miniMapWidth - fluff) && pos.Y > (Main.miniMapY + fluff) && pos.Y < (Main.miniMapY + Main.miniMapHeight - fluff);
+
+		/// <summary>Gets the position of something on the minimap given the tile coordinates.</summary>
+		/// <param name="scale"></param>
+		/// <param name="tilePos"></param>
+		/// <returns></returns>
+		private static Vector2 GetMiniMapPosition(Vector2 tilePos, float scale = 1f)
+		{
+			float screenHalfWidthInTiles = (Main.screenPosition.X + (PlayerInput.RealScreenWidth / 2f)) / 16f;
+			float screenHalfHeightInTiles = (Main.screenPosition.Y + (PlayerInput.RealScreenHeight / 2f)) / 16f;
+			float offsetX = screenHalfWidthInTiles - (Main.miniMapWidth / scale) / 2f;
+			float offsetY = screenHalfHeightInTiles - (Main.miniMapHeight / scale) / 2f;
+
+			float drawPosX = (tilePos.X - offsetX) * scale;
+			float drawPosY = (tilePos.Y - offsetY) * scale;
+
+			Vector2 miniMapSize = new Vector2(Main.miniMapX, Main.miniMapY);
+			drawPosX += miniMapSize.X;
+			drawPosY += miniMapSize.Y;
+			drawPosY -= 2f * scale / 5f;
+
+			return new Vector2(drawPosX, drawPosY);
+		}
+
+		public static void DrawOnMinimap(int posX, int posY, float scale, Texture2D tex)
+		{
+			Vector2 scrPos = new Vector2(posX, posY);
+			Main.spriteBatch.Draw(
+				texture: tex,
+				position: scrPos,
+				sourceRectangle: null,
+				color: Color.White,
+				rotation: 0f,
+				origin: new Vector2(tex.Width / 2, tex.Height / 2),
+				scale: scale,
+				effects: SpriteEffects.None,
+				layerDepth: 1f
+			);
 		}
 
 		public static void Unload()
@@ -148,9 +221,7 @@ namespace SpiritMod.Utilities
 				for (int j = 0; j < len; j++)
 				{
 					if (Main.chatLine[i].parsedText[j] is UI.Chat.QuestTagHandler.QuestSnippet snip)
-					{
 						snip.PostUpdate();
-					}
 				}
 			}
 		}
