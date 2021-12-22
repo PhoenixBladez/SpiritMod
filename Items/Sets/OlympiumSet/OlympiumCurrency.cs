5f7fd8c012b08968d9fb42f906095c92901b2f47
GabeHasWon <gabriel@guevarameyer.com>
2021-12-21 20:23:15 -0600

    oracle progress + olympium currency

---
+++ Items/Sets/OlympiumSet/OlympiumCurrency.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.GameContent.UI;
+using Terraria.Localization;
+
+namespace SpiritMod.Items.Sets.OlympiumSet
+{
+	class OlympiumCurrency : CustomCurrencySingleCoin
+	{
+		public static readonly Color TextColour = Color.Goldenrod;
+
+		public OlympiumCurrency(int coinItemID, long currencyCap) : base(coinItemID, currencyCap) { }
+
+		public override void GetPriceText(string[] lines, ref int currentLine, int price)
+		{
+			Color color = TextColour * (Main.mouseTextColor / 255f);
+			lines[currentLine++] = $"[c/{color.R}{color.G}{color.B}:{Language.GetTextValue("LegacyTooltip.50")} {price} tokens]";
+		}
+	}
+}
