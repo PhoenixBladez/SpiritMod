47e4ff2687decf7ded9eb15c65da71d66207914e
GabeHasWon <gabriel@guevarameyer.com>
2021-12-28 15:00:49 -0600

    Warden barely started + bg system for warden

---
+++ NPCs/StarjinxEvent/Enemies/Warden/WardenBG.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.Xna.Framework;
+using SpiritMod.Mechanics.BackgroundSystem.BGItem;
+using Terraria;
+using Terraria.ModLoader;
+
+namespace SpiritMod.NPCs.StarjinxEvent.Enemies.Warden
+{
+	class WardenBG : BaseBGItem
+	{
+		private readonly NPC Parent;
+
+		private Warden WardenParent => Parent.modNPC as Warden;
+
+		public WardenBG(Vector2 pos, NPC parent) : base(pos, 0f, new Point(0, 0))
+		{
+			tex = ModContent.GetTexture("SpiritMod/NPCs/StarjinxEvent/Enemies/Warden/WardenBG");
+			source = new Rectangle(0, 0, tex.Width, tex.Height);
+			scale = 1f;
+
+			Parent = parent;
+		}
+
+		internal override void Behaviour()
+		{
+			Center = Parent.Center;
+
+			scale = .5f;
+			BaseParallax(.75f);
+		}
+
+		internal override void Draw(Vector2 off)
+		{
+			if (WardenParent.inFG)
+				return;
+
+			drawColor = Color.Lerp(Main.bgColor, Color.White, 0.5f);
+			base.Draw(GetParallax());
+		}
+	}
+}
