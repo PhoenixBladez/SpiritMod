12edf6f0efc0b85080d915295d7d16c52b9c36a3
GraydeeSi <wiseguy0002@gmail.com>
2022-01-01 14:35:02 -0500

    Gtech grenade basically finished

--- Items/Sets/GranitechSet/GtechGrenade/GtechGrenade.cs
+++ Items/Sets/GranitechSet/GtechGrenade/GtechGrenade.cs
@@ -7,17 +7,22 @@ using Terraria.ModLoader;
 using System.Linq;
 using SpiritMod.Dusts;
 using SpiritMod.Items.Material;
+using SpiritMod.Buffs;
 using SpiritMod.Utilities;
 
 namespace SpiritMod.Items.Sets.GranitechSet.GtechGrenade
 {
 	public class GtechGrenade : ModItem
 	{
-		public override void SetStaticDefaults() => DisplayName.SetDefault("G-TEK Grenade");
+		public override void SetStaticDefaults()
+		{
+			DisplayName.SetDefault("G-TEK Grenade");
+			Tooltip.SetDefault("Slows and electrocutes enemies in it's aura \nCan be destroyed by the player \nDestroying it causes an explosion");
+		}
 
 		public override void SetDefaults()
 		{
-			item.damage = 60;
+			item.damage = 70;
 			item.width = 40;
 			item.height = 40;
 			item.useTime = 20;
@@ -55,8 +60,8 @@ namespace SpiritMod.Items.Sets.GranitechSet.GtechGrenade
 			projectile.penetrate = -1;
 			projectile.tileCollide = false;
 			projectile.hostile = false;
-			projectile.friendly = true;
-			projectile.width = projectile.height = 20;
+			projectile.friendly = false;
+			projectile.width = projectile.height = 32;
 			projectile.timeLeft = 400;
 		}
 		public override void AI()
@@ -75,8 +80,86 @@ namespace SpiritMod.Items.Sets.GranitechSet.GtechGrenade
 						projectile.frame = 7;
 				}
 			}
+			if (projectile.velocity.Length() < 2)
+			{
+				float rotationOffset = damageAura ? 0 : (float)Math.Sin(projectile.frameCounter * 0.5f) * MathHelper.Lerp(0.05f, 0.02f, projectile.frameCounter / 25f);
+				projectile.rotation = MathHelper.Lerp(projectile.rotation, 6.28f, 0.2f) + rotationOffset;
+			}
+			else
+			{
+				projectile.rotation += projectile.velocity.Length() * 0.04f;
+				projectile.rotation %= 6.28f;
+			}
 			if (damageAura)
+			{
 				Lighting.AddLight(projectile.Center, Color.Cyan.ToVector3());
+				foreach (NPC npc in Main.npc)
+				{
+					if (npc.townNPC || !npc.active || npc.immortal)
+						continue;
+					if (InAura(npc.Hitbox))
+					{
+						npc.AddBuff(ModContent.BuffType<ElectrifiedV2>(), 10);
+						if (!npc.boss)
+							npc.AddBuff(ModContent.BuffType<MageFreeze>(), 10);
+					}
+				}
+			}
+
+			if (damageAura && CheckHit() && projectile.timeLeft > 18)
+			{
+				projectile.timeLeft = 18;
+				for (int i = 0; i < 6; i++)
+					Dust.NewDustPerfect(projectile.Center, DustID.Electric);
+			}
+
+			if (projectile.timeLeft < 18)
+				projectile.scale = Math.Max((projectile.timeLeft - 5) / 13f, 0);
+		}
+
+		private bool CheckHit()
+		{
+			foreach (Projectile proj in Main.projectile)
+			{
+				if (!proj.active || !proj.friendly || proj == projectile || proj.type == projectile.type)
+					continue;
+				if (proj.modProjectile == null)
+				{
+					if (proj.Hitbox.Intersects(projectile.Hitbox))
+						return true;
+				}
+				else
+				{
+					bool? colliding = proj.modProjectile.Colliding(proj.Hitbox, projectile.Hitbox);
+					if (colliding == null)
+					{
+						if (proj.Hitbox.Intersects(projectile.Hitbox))
+							return true;
+					}
+					else
+						return (bool)colliding;
+				}
+			}
+
+			return false;
+		}
+
+		private bool InAura(Rectangle targetHitbox)
+		{
+			if (!damageAura)
+				return false;
+			float collisionPoint = 0f;
+			for (float i = 0; i < 6.28f; i += 0.78f)
+			{
+				if (Collision.CheckAABBvLineCollision(targetHitbox.TopLeft(), targetHitbox.Size(), projectile.Center, projectile.Center + (i.ToRotationVector2() * 50 * projectile.scale), projectile.width, ref collisionPoint))
+					return true;
+			}
+			return false;
+		}
+
+		public override void Kill(int timeLeft)
+		{
+			Projectile.NewProjectile(projectile.Center, Vector2.Zero, ModContent.ProjectileType<GtechGrenadeExplode>(), projectile.damage, 0, projectile.owner);
 		}
 
 		public override bool PreDraw(SpriteBatch spriteBatch, Color lightColor)
@@ -108,6 +191,63 @@ namespace SpiritMod.Items.Sets.GranitechSet.GtechGrenade
 					projectile.rotation, projectile.DrawFrame().Size() / 2, projectile.scale, SpriteEffects.None, 0);
 			});
 	
+			return false;
+		}
+	}
+
+	public class GtechGrenadeExplode : ModProjectile
+	{
+		private bool damaging => projectile.frame >= 2 && projectile.frame < 4;
+		public override void SetStaticDefaults()
+		{
+			DisplayName.SetDefault("Gtech Grenade");
+			Main.projFrames[projectile.type] = 9;
+		}
+
+		public override void SetDefaults()
+		{
+			projectile.penetrate = -1;
+			projectile.tileCollide = false;
+			projectile.hostile = false;
+			projectile.friendly = true;
+			projectile.width = projectile.height = 20;
+			projectile.timeLeft = 400;
+		}
+
+		public override void AI()
+		{
+			Lighting.AddLight(projectile.Center, Color.Cyan.ToVector3());
+			projectile.frameCounter++;
+			if (projectile.frameCounter % 5 == 0)
+			{
+				projectile.frame++;
+				if (projectile.frame >= Main.projFrames[projectile.type])
+					projectile.active = false;
+			}
+		}
+		public override bool? Colliding(Rectangle projHitbox, Rectangle targetHitbox)
+		{
+			if (!damaging)
+				return false;
+			float collisionPoint = 0f;
+			for (float i = 0; i < 6.28f; i += 0.392f)
+			{
+				if (Collision.CheckAABBvLineCollision(targetHitbox.TopLeft(), targetHitbox.Size(), projectile.Center, projectile.Center + (i.ToRotationVector2() * 114), projectile.width, ref collisionPoint))
+					return true;
+			}
+			return false;
+		}
+
+		public override bool PreDraw(SpriteBatch spriteBatch, Color lightColor)
+		{
+
+			Texture2D tex = Main.projectileTexture[projectile.type];
+			int frameHeight = tex.Height / Main.projFrames[projectile.type];
+			Rectangle frame = new Rectangle(0, frameHeight * projectile.frame, tex.Width, frameHeight);
+			DrawAberration.DrawChromaticAberration(Vector2.UnitY, 2f, delegate (Vector2 offset, Color colorMod)
+			{
+				spriteBatch.Draw(tex, projectile.Center + offset - Main.screenPosition, frame, Color.White.MultiplyRGBA(colorMod), projectile.rotation, new Vector2(tex.Width, frameHeight) / 2, projectile.scale, SpriteEffects.None, 0f);
+			});
 			return false;
 		}
 	}
