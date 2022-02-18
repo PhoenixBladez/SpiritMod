be74a82fdfb7cbd2454a82c9b73bb58a31d3a879
GabeHasWon <gabriel@guevarameyer.com>
2022-02-17 19:36:46 -0600

    compulsive cleanup

--- Items/Accessory/OpalFrog/OpalFrog_Tile.cs
+++ Items/Accessory/OpalFrog/OpalFrog_Tile.cs
@@ -1,4 +1,3 @@
-
 using Microsoft.Xna.Framework;
 using Terraria;
 using Terraria.Enums;
@@ -19,11 +18,9 @@ namespace SpiritMod.Items.Accessory.OpalFrog
 			TileObjectData.newTile.Width = 2;
 			TileObjectData.newTile.CoordinateHeights = new int[] { 16, 16};
 			TileObjectData.newTile.Direction = TileObjectDirection.PlaceLeft;
-			TileObjectData.newTile.StyleWrapLimit = 2; //not really necessary but allows me to add more subtypes of chairs below the example chair texture
-			TileObjectData.newTile.StyleMultiplier = 2; //same as above
 			TileObjectData.newTile.StyleHorizontal = true;
 			TileObjectData.newAlternate.CopyFrom(TileObjectData.newTile);
-			TileObjectData.newAlternate.Direction = TileObjectDirection.PlaceRight; //allows me to place example chairs facing the same way as the player
+			TileObjectData.newAlternate.Direction = TileObjectDirection.PlaceRight;
 			TileObjectData.addAlternate(1); //facing right will use the second texture style
 			TileObjectData.addTile(Type);
 			ModTranslation name = CreateMapEntryName();
@@ -31,15 +28,8 @@ namespace SpiritMod.Items.Accessory.OpalFrog
 			dustType = -1;
 			AddMapEntry(new Color(200, 200, 200), name);
 		}
-		public override void SetDrawPositions(int i, int j, ref int width, ref int offsetY, ref int height)
-		{
-			offsetY = 2;
-		}
-		public override void KillMultiTile(int i, int j, int frameX, int frameY)
-		{
-			Item.NewItem(i * 16, j * 16, 48, 48, ModContent.ItemType<OpalFrogItem>());
 
-		}
-
+		public override void SetDrawPositions(int i, int j, ref int width, ref int offsetY, ref int height) => offsetY = 2;
+		public override void KillMultiTile(int i, int j, int frameX, int frameY) => Item.NewItem(i * 16, j * 16, 48, 48, ModContent.ItemType<OpalFrogItem>());
 	}
 }
