c66176e37d629a6ed99d360e396c202ed2a575bb
PhoenixBladez <phoenixbladeyt@gmail.com>
2022-03-20 22:35:07 -0700

    Claw Cannon Polish

--- Items/Sets/ReefhunterSet/ClawCannon.cs
+++ Items/Sets/ReefhunterSet/ClawCannon.cs
@@ -16,10 +16,10 @@ namespace SpiritMod.Items.Sets.ReefhunterSet
 
 		public override void SetDefaults()
 		{
-			item.damage = 36;
+			item.damage = 18;
 			item.width = 38;
 			item.height = 26;
-			item.useTime = item.useAnimation = 35;
+			item.useTime = item.useAnimation = 37;
 			item.mana = 9;
 			item.useStyle = ItemUseStyleID.HoldingOut;
 			item.knockBack = 4;
@@ -29,20 +29,27 @@ namespace SpiritMod.Items.Sets.ReefhunterSet
 			item.autoReuse = true;
 			item.noMelee = true;
 			item.magic = true;
-			item.shootSpeed = 16f;
+			item.shootSpeed = 15f;
 			item.UseSound = SoundID.Item20;
 			item.shoot = ModContent.ProjectileType<Cannonbubble>();
 		}
 
 		public override bool Shoot(Player player, ref Vector2 position, ref float speedX, ref float speedY, ref int type, ref int damage, ref float knockBack)
 		{
+			Main.PlaySound(SoundID.Item, (int)position.X, (int)position.Y, 85);
+
 			for (int i = 0; i < 10; ++i)
+			{
 				Dust.NewDust(position, 0, 0, DustID.BubbleBurst_Blue, speedX * Main.rand.NextFloat(0.65f, 1f), speedY * Main.rand.NextFloat(0.65f, 1f), 0, default, Main.rand.NextFloat(0.75f, 1.5f));
 
+			}
 			player.velocity -= new Vector2(speedX, speedY) * 0.15f;
 			return true;
 		}
-
+		public override Vector2? HoldoutOffset()
+		{
+			return new Vector2(-10, 0);
+		}
 		//public override void AddRecipes()
 		//{
 		//	ModRecipe modRecipe = new ModRecipe(mod);
