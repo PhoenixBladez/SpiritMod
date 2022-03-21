81845f4fac1f35245937eb4d2245ac1e59fba927
GabeHasWon <gabriel@guevarameyer.com>
2022-03-21 14:01:39 -0500

    empowered swim fix

--- Items/Sets/ReefhunterSet/Buffs/EmpoweredSwim.cs
+++ Items/Sets/ReefhunterSet/Buffs/EmpoweredSwim.cs
@@ -18,8 +18,13 @@ namespace SpiritMod.Items.Sets.ReefhunterSet.Buffs
 
 			if (player.buffTime[buffIndex] > 2)
 			{
-				player.fullRotationOrigin = player.Size / 2f;
-				player.fullRotation = player.velocity.ToRotation() + Microsoft.Xna.Framework.MathHelper.PiOver2;
+				if (player.wet && !Collision.SolidCollision(player.BottomLeft, player.width, 4))
+				{
+					player.fullRotationOrigin = player.Size / 2f;
+					player.fullRotation = player.velocity.ToRotation() + Microsoft.Xna.Framework.MathHelper.PiOver2;
+				}
+				else
+					player.fullRotation *= 0.8f;
 			}
 			else
 			{
