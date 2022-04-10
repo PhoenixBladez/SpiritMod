0f1eeca9720756201dadbbac92ba214175a66f84
GabeHasWon <gabriel@guevarameyer.com>
2022-04-09 22:20:11 -0500

    captains regards resprite

--- Items/Sets/GunsMisc/CaptainsRegards/CaptainsRegards.cs
+++ Items/Sets/GunsMisc/CaptainsRegards/CaptainsRegards.cs
@@ -7,27 +7,25 @@ namespace SpiritMod.Items.Sets.GunsMisc.CaptainsRegards
 {
 	public class CaptainsRegards : ModItem
 	{
-		private Vector2 newVect;
 		public override void SetStaticDefaults()
 		{
 			DisplayName.SetDefault("Captain's Regards");
 			Tooltip.SetDefault("'Pirate diplomacy at its finest'");
 		}
 
-
 		public override void SetDefaults()
 		{
 			item.damage = 20;
 			item.ranged = true;
 			item.width = 65;
-			item.height = 21;
-			item.useTime = 22;
+			item.height = 62;
+			item.useTime = 24;
 			item.useAnimation = 22;
 			item.useStyle = ItemUseStyleID.HoldingOut;
 			item.noMelee = true;
 			item.knockBack = 6;
 			item.useTurn = false;
-			item.value = Terraria.Item.sellPrice(0, 5, 0, 0);
+			item.value = Item.sellPrice(0, 5, 0, 0);
 			item.rare = ItemRarityID.Pink;
 			item.UseSound = SoundID.Item36;
 			item.autoReuse = true;
@@ -43,23 +41,17 @@ namespace SpiritMod.Items.Sets.GunsMisc.CaptainsRegards
 			newProj.friendly = true;
 			newProj.hostile = false;
 			Vector2 origVect = new Vector2(speedX, speedY);
-			for (int X = 0; X <= 2; X++) {
-				if (Main.rand.Next(2) == 1) {
-					newVect = origVect.RotatedBy(System.Math.PI / (Main.rand.Next(82, 1800) / 10));
-				}
-				else {
-					newVect = origVect.RotatedBy(-System.Math.PI / (Main.rand.Next(82, 1800) / 10));
-				}
+
+			for (int X = 0; X < 3; X++)
+			{
+				Vector2 newVect = origVect.RotatedBy(-System.Math.PI / (Main.rand.Next(82, 1800) / 10) * (Main.rand.NextBool() ? -1 : 1));
 				int proj2 = Projectile.NewProjectile(position.X, position.Y, newVect.X, newVect.Y, type, 45, knockBack, player.whoAmI);
 				Projectile newProj2 = Main.projectile[proj2];
 				newProj2.timeLeft = Main.rand.Next(13, 30);
 			}
 			return false;
 		}
-		public override Vector2? HoldoutOffset()
-		{
-			return new Vector2(-10, 0);
-		}
 
+		public override Vector2? HoldoutOffset() => new Vector2(-10, 0);
 	}
 }
